b63b15962614e2edac386fa60809bfd644dc155a
Thomas Vandivier <tvandivi@e1z2r2p6.42.us.org>
2019-10-01 15:56:14 -0700

    updating other files

--- ft_ssl.h
+++ ft_ssl.h
@@ -2,8 +2,7 @@
 # define FT_SSL_H
 
 # include <math.h>
-# include <unistd.h>
-# include "../vogsphere_ftprintf/includes/ft_printf.h"
+# include "libft/libft.h"
 
 /*
  ** rps: specifies sround per shift
@@ -42,51 +41,35 @@
 # define FT_UNDERLINE		"\033[4m"
 # define FT_END_ATTR		"\033[0m"
 
-# define FF(b, c, d) (d ^ (b & (c ^ d)))
-# define FG(b, c, d) FF (d, b, c)
-# define FH(b, c, d) (b ^ c ^ d)
-# define FI(b, c, d) (c ^ (b | ~d))
-
-typedef struct		s_list
-{
-	void			*content;
-	size_t			content_size;
-	struct s_list	*next;
-}					t_list;
+typedef unsigned char	ft_uchar;
+typedef unsigned long	ft_ulong;
+typedef	long long		ft_llong;
 
 typedef struct		s_ft_getopt
 {
+	char			flag[2];
 	int				i;
 	int				c;
-    char			*ft_md5;
-	char			*ft_sha256;
+    ft_uchar		*ft_md5;
+	ft_uchar		*ft_sha256;
 }					t_getopt;
 
-typedef unsigned Digest[4];
-typedef unsigned char *u_int32_ssl;
-
-typedef struct	s_ft_md5
+typedef struct		s_ft_md5
 {
-	int     		k[64];
-	int         	rps[64];
-	int         	a0;
-	int         	b0;
-	int         	c0;
-	int         	d0;
-	int				l; /* (= msg.length/4 + 2)  length (int 32-bit integers) of msg + '1' + appended length */
-	int				n;
-	unsigned int	**m;
-	unsigned char  	*message;
-}               	t_md5;
+	unsigned long	i[2];
+	unsigned long	buf[4];
+	unsigned char	in[64];
+	unsigned char	digest[16];
+	unsigned char	*msg;
+	unsigned int	size;
+}               	t_md5_ctx;
 
-unsigned			*ft_md5(const char *msg, int mlen);
-unsigned			*ft_sha256(const char *msg, int mlen);
+unsigned char		*ft_md5(const unsigned char *d, unsigned long n, unsigned char *md);
 void				ft_ssl_message(char *invalid);
 void				ft_ssl_free_optins(t_getopt *glb_opt);
 void				ft_ssl_preform_action(t_getopt *glb_opt, int ac, char **av);
 int					ft_getopt(int ac, char **av, t_getopt *glb_opt);
 void				ft_opt_init(t_getopt *glb_opt);
 int					ft_scmp(const char *s1, const char *s2);
-int					get_md5_buffer(t_md5 *md5);
 
 #endif
--- makefile
+++ makefile
@@ -1,16 +1,13 @@
 NAME= ft_ssl
 
-FLAGSd= -Wall -Werror -Wextra
+FLAGS= -Wall -Werror -Wextra
 
-DEBUG= -g
-
-SRC= $(addsuffix .c, ft_scmp ft_md5 ft_sha256 ft_ssl_utils_1 ft_ssl_utils_2 main)
+SRC= $(addsuffix .c, ft_ssl_utils ft_md5 main)
 
 $(NAME):
 	@echo "\033[0;32mBuilding FT_SSL Program\033[0m"
-	@make -C ftprintf
-	@make -C ftprintf clean
-	@$(CC) $(SRC) ftprintf/libftprintf.a -o $(NAME)
+	@make -C libft
+	@$(CC) $(SRC) libft/libft.a -o $(NAME)
 
 all: $(NAME)
 
@@ -18,9 +15,10 @@ clean:
 	@rm ft_ssl 2>/dev/null && echo "\033[0;36m\tremoving ft_ssl...\033[0m" || echo "\033[0;36m\tchecking for ft_ssl removal...\nremoved \033[0;32m\tX\033[0m"
 	@rm ft_ssl_debug 2>/dev/null && echo "\033[0;36m\tremoving ft_ssl_debug...\033[0m" || echo "\033[0;36m\tchecking for ft_ssl_debug removal...\nremoved \033[0;32m\tX\033[0m"
 	@rm -rf ft_ssl_debug.dSYM 2>/dev/null && echo "\033[0;36m\tremoving ft_ssl_debug.dSYM...\033[0m" || echo "\033[0;36m\tchecking for ft_ssl_debug.dSYM removal...\nremoved \033[0;32m\tX\033[0m"
-	@make -C ftprintf fc
+	@make -C libft clean
 
 fclean: clean
+	@make -C libft fclean
 
 fc: fclean
 	@echo "\033[1;34mDestroying FT_SSL Program\033[0m"
@@ -28,8 +26,8 @@ fc: fclean
 re: fc all
 
 debug: fc
-	@make -C ftprintf
-	@make -C ftprintf clean
-	@$(CC) $(DEBUG) $(FLAGS) $(SRC) ftprintf/libftprintf.a -o ft_ssl_debug
+	@echo "\033[0;32mBuilding FT_SSL \033[0;34mDEBUG MODE\033[0m"
+	@make -C libft
+	@$(CC) -g $(SRC) libft/libft.a -o ft_ssl_debug
 
 .PHONY: clean fclean fc re main.c ft_ssl
